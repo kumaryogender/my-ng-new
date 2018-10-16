200b99fa2cd84c31c8398127fe8db20794faed5a
yogender yadav <Yogender.Yadav1@T-Mobile.com>
2018-10-16 16:42:23 +0530

    updated html title

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HeroComponent } from './hero/hero.component';
+
+const routes: Routes = [
+  { path: 'hero', component: HeroComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
